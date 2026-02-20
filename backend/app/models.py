4bbb72ff1bf7bc02291ae412809db881eb8d53c0
roshanshrishrimal24 <250133107023@gecg28.ac.in>
2026-02-20 23:27:39 +0530

    Create backend/app/models.py with SQLAlchemy models User and Product

---
+++ backend/app/models.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from database import Base
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+    products = relationship('Product', back_populates='owner')
+
+class Product(Base):
+    __tablename__ = 'products'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    price = Column(Integer)
+    owner_id = Column(Integer, ForeignKey('users.id'))
+
+    owner = relationship('User', back_populates='products')
